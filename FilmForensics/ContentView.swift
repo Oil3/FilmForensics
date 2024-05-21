1de002f73518b1e21e8e2df9180177949c7e075e
oil3 <3oiltree@gmail.com>
2024-05-20 20:04:10 -0500

    Initial Commit

---
+++ FilmForensics/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FilmForensics
+//
+//  Created by Almahdi Morris on 05/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FilmForensics/FilmForensicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmForensicsApp.swift
+//  FilmForensics
+//
+//  Created by Almahdi Morris on 05/20/24.
+//
+
+import SwiftUI
+
+@main
+struct FilmForensicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
