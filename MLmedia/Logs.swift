6ebbefc8ac54d2db1ac6716d7fd4a23b7a953c3e
oil3 <3oiltree@gmail.com>
2024-06-22 02:53:57 -0500

    mvpp

--- MLmedia/Logs.swift
+++ MLmedia/Logs.swift
@@ -7,18 +7,48 @@
 import SwiftUI
 
 struct DetectionLog: Codable {
-    let videoURL: String
-    let creationDate: String
-    var frames: [FrameLog]
+  var videoURL: String
+  var creationDate: String
+  var frames: [FrameLog]
+  
+  enum CodingKeys: String, CodingKey {
+    case videoURL
+    case creationDate
+    case frames
+    case metadata
+  }
+  
+  struct Metadata: Codable {
+    var videoFilename: String
+    var modelFilename: String
+    var totalFrames: Int
+    var totalFramesWithDetections: Int
+    var totalFramesWithoutDetections: Int
+    var totalObjectsDetected: Int
+    var totalObjectsBelowThreshold: Int
+    var averageConfidence: Float
+    var averageDetectionSpeed: Float
+    var frameNumbersNotAnalysed: [Int]
+  }
+  
+  var metadata: Metadata?
 }
 
+
 struct FrameLog: Codable {
-    let frameNumber: Int
-    let detections: [Detection]
+  var frameNumber: Int
+  var detections: [Detection]?
 }
 
 struct Detection: Codable {
-    let boundingBox: CGRect
-    let identifier: String?
-    let confidence: Float
+  var boundingBox: CGRect
+  var identifier: String
+  var confidence: Float
 }
+extension CGFloat {
+  func rounded(toPlaces places: Int) -> CGFloat {
+    let divisor = pow(10.0, CGFloat(places))
+    return (self * divisor).rounded() / divisor
+  }
+}
+
